29380789d4abe433097903419b9dc723686385c4
VladimirVeraksa <49755858+baban666@users.noreply.github.com>
2021-01-20 19:44:00 +0100

    feat: add update data final

--- wp-favorit-widget.php
+++ wp-favorit-widget.php
@@ -5,9 +5,8 @@
 
 
 
-
-
 add_action('widgets_init', 'wfm_cats');
+
 
 
 function wfm_cats(){
@@ -22,11 +21,49 @@ class WFM_Cats extends WP_Widget {
      */
     public function __construct() {
         $widget_ops = array(
-            'name' => 'Favorits category',
+	        'classname' => 'WFM_Cats',
+            'name' => 'Favorit category',
             'description' => 'My Widget is awesome',
         );
-        parent::__construct( 'wfm-cats','Favorits category', $widget_ops );
+        parent::__construct( 'WFM_Cats','Favorits category', $widget_ops );
     }
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// var_dump($instance);
+		$count = isset($instance['count']) ? $instance['count'] : 5;
+		$cats = get_categories();
+		// print_r($cats )
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id('count') ?>">Count posts</label>
+			<input id="<?php  echo $this->get_field_id('count') ?>"
+			       name="<?php  echo $this->get_field_name('count') ?>"
+			       type="text"
+			       value="<?php echo $count; ?>"
+			       class="widefat">
+		</p>
+
+		<?php echo '<p>'; ?>
+			<?php foreach ($cats as $cat): ?>
+					<input
+						id="<?php echo $this->get_field_id('cat') . $cat->cat_ID;?>"
+						name="<?php  echo $this->get_field_name('cat'); ?>[]"
+						type="checkbox"
+						value="<?php echo $cat->cat_ID; ?>"
+						<?php if(is_array($instance['cat']) && in_array($cat->cat_ID, $instance['cat']))  echo 'checked'; ?>
+					>
+					<label for="<?php echo $this->get_field_id('cat') . $cat->cat_ID?>"><?php echo $cat->name ?></label><br>
+			<?php endforeach; ?>
+       <?php echo '<p>'; ?>
+
+
+<?php
+	}
 
     /**
      * Outputs the content of the widget
@@ -36,15 +73,31 @@ class WFM_Cats extends WP_Widget {
      */
     public function widget( $args, $instance ) {
         // outputs the content of the widget
-    }
+	    if(!empty($instance)){
+	    	foreach ($instance['cat'] as $cat_id){
+			    $cat = get_category($cat_id);
+			    global $post;
+			   $posts = get_posts(array(
+			   	'category' => $cat_id,
+				 'numberposts'  => $instance['count']
+			   ));
 
-    /**
-     * Outputs the options form on admin
-     *
-     * @param array $instance The widget options
-     */
-    public function form( $instance ) {
-        // outputs the options form on admin
+			   echo '<div class="widget">';
+			   echo "<h6>{$cat->name}<h2>";
+			   echo '<ul>';
+			   foreach ($posts as $post){
+			   	setup_postdata($post);
+			   	echo '<li><a href="'. get_permalink() .'">'. get_the_title() . '</a></li>';
+			   }
+
+			   echo '</ul>';
+			   echo '</div>';
+			   // var_dump($posts);
+			    wp_reset_postdata();
+
+		    }
+	    }
+	    //print_r($args);
     }
 
     /**
@@ -57,5 +110,10 @@ class WFM_Cats extends WP_Widget {
      */
     public function update( $new_instance, $old_instance ) {
         // processes widget options to be saved
+	    $new_instance['count'] = ((int) $new_instance['count']) ? abs($new_instance['count']) : 5;
+	    foreach ($new_instance['cat'] as $k => $v){
+		    $new_instance['cat'][$k] = (int)$v;
+	    }
+	    return $new_instance;
     }
 }

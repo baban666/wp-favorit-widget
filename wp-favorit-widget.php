3fb43e1508986ce76344133e5a79377dd2503fc6
VladimirVeraksa <49755858+baban666@users.noreply.github.com>
2021-01-20 13:17:22 +0100

    feat: add widget class

---
+++ wp-favorit-widget.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Plugin Name: WFM Cats
+ */
+
+
+
+
+
+add_action('widgets_init', 'wfm_cats');
+
+
+function wfm_cats(){
+    register_widget('WFM_Cats');
+}
+
+
+class WFM_Cats extends WP_Widget {
+
+    /**
+     * Sets up the widgets name etc
+     */
+    public function __construct() {
+        $widget_ops = array(
+            'name' => 'Favorits category',
+            'description' => 'My Widget is awesome',
+        );
+        parent::__construct( 'wfm-cats','Favorits category', $widget_ops );
+    }
+
+    /**
+     * Outputs the content of the widget
+     *
+     * @param array $args
+     * @param array $instance
+     */
+    public function widget( $args, $instance ) {
+        // outputs the content of the widget
+    }
+
+    /**
+     * Outputs the options form on admin
+     *
+     * @param array $instance The widget options
+     */
+    public function form( $instance ) {
+        // outputs the options form on admin
+    }
+
+    /**
+     * Processing widget options on save
+     *
+     * @param array $new_instance The new options
+     * @param array $old_instance The previous options
+     *
+     * @return array
+     */
+    public function update( $new_instance, $old_instance ) {
+        // processes widget options to be saved
+    }
+}
